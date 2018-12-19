53442d779e3e42b841050aea338e68b2d92856e9
pk69mangpong <45988188+pk69mangpong@users.noreply.github.com>
2018-12-19 18:23:33 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,15 +5,6 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
-});
-
 client.on('message', message => {
     if (message.content === 'น้องเมด') {
     	message.reply('คะ...นายท่าน...น้องเมดยินดีรับใช้ค่ะ');
