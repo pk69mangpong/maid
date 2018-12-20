83ba68b1d9f5f370ccf6ae14b9e59516a5808433
pk69mangpong <45988188+pk69mangpong@users.noreply.github.com>
2018-12-20 09:45:36 +0700

    test
    client.on('guildMemberAdd', member => {
        member.guild.channels.get('524513951736922125').send("Welcome"); 
    });

--- bot.js
+++ bot.js
@@ -11,9 +11,36 @@ client.on('message', message => {
   	}
 });
 
-client.on('guildMemberAdd', member => {
-    member.guild.channels.get('524513951736922125').send("Welcome"); 
-});
+client.on('guildMemberAdd', msg => { // Commands Go Inside The client.on('message', 
+msg => )
+msg.guild.channels.get('524513951736922125').send({embed: {
+color: 3447003,
+author: {
+  name: client.user.username,
+  icon_url: client.user.avatarURL
+},
+title: "Welcome To ()!",
+url: "https://districtservices.net",
+description: "@MEMBER",
+fields: [{
+    name: "Fields",
+    value: "They can have different fields with small headlines."
+  },
+  {
+    name: "Masked links",
+    value: "You can put [masked links](http://google.com) inside of rich embeds."
+  },
+  {
+    name: "Markdown",
+    value: "You can put all the *usual* **__Markdown__** inside of them."
+  }
+],
+timestamp: new Date(),
+footer: {
+  icon_url: client.user.avatarURL,
+  text: "© Example"
+}
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
