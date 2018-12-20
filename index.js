b0323c22b82aab5ac5a7a5a1f8f67985b08384f9
pk69mangpong <45988188+pk69mangpong@users.noreply.github.com>
2018-12-20 08:33:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ client.on("ready", () => {
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
+client.on('message', message => {
+    if (message.content === 'testmaid') {
+    	message.reply('คะ?');
+  	}
+});
+
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
